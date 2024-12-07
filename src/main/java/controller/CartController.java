5cf683aabaf4a464ec4dcb08b385e38fa45a563b
21110862@student.hcmute.edu.vn <21110862@student.hcmute.edu.vn>
2024-12-07 15:04:46 +0700

    cartController

---
+++ src/main/java/controller/CartController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+import model.CategoryModel;
+import service.CategoryService;
+import serviceImpl.CategoryServiceImpl;
+
+@WebServlet(urlPatterns = { "/cart" })
+public class CartController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	CategoryService categoryservice = new CategoryServiceImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		req.setCharacterEncoding("UTF-8");
+		List<CategoryModel> allCategory = categoryservice.findAll();
+		req.setAttribute("allcategory", allCategory);
+		
+		RequestDispatcher rq = req.getRequestDispatcher("views/cart.jsp");
+		rq.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
