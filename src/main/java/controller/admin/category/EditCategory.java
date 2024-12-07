e714b393404d7b13fead6aeab58f05c32d66e96d
21110862@student.hcmute.edu.vn <21110862@student.hcmute.edu.vn>
2024-12-07 22:00:20 +0700

    EditCategory and ListCategory

---
+++ src/main/java/controller/admin/category/EditCategory.java
@@ -0,0 +1,37 @@
+package controller.admin.category;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.CategoryModel;
+import service.CategoryService;
+import serviceImpl.CategoryServiceImpl;
+import utils.FormUtil;
+
+@WebServlet(urlPatterns = { "/admin-category-edit" })
+
+public class EditCategory extends HttpServlet  {
+
+	private static final long serialVersionUID = 1L;
+	CategoryService categoryService = new CategoryServiceImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		CategoryModel model = FormUtil.toModel(CategoryModel.class, request);
+		if(model.getCategoryID() != null) {
+			request.setAttribute("categoryEdit",categoryService.getOne(model.getCategoryID()));
+		}
+		request.setAttribute("model",model);
+		RequestDispatcher rq = request.getRequestDispatcher("views/admin/category/editcategory.jsp");
+		rq.forward(request, resp);
+	}
+}
---
+++ src/main/java/controller/admin/category/ListCategory.java
@@ -0,0 +1,43 @@
+package controller.admin.category;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import constant.SystemConstant;
+import model.CategoryModel;
+import paging.PageRequest;
+import paging.Pageble;
+import service.CategoryService;
+import serviceImpl.CategoryServiceImpl;
+import utils.FormUtil;
+
+@WebServlet(urlPatterns = { "/admin-category-list" })
+
+public class ListCategory extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	CategoryService categoryservice = new CategoryServiceImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		CategoryModel model = FormUtil.toModel(CategoryModel.class, request);
+		Pageble pageble = new PageRequest(model.getPage(), model.getMaxPageItem());
+		List<CategoryModel> allCategory = categoryservice.findAllPaging(pageble);
+		model.setTotalItem(categoryservice.getTotalItem());
+		model.setTotalPage((int) Math.ceil((double) model.getTotalItem()/ model.getMaxPageItem()));
+		request.setAttribute("allcategory", allCategory);
+		request.setAttribute(SystemConstant.MODEL, model);
+		RequestDispatcher rq = request.getRequestDispatcher("views/admin/category/listcategory.jsp");
+		rq.forward(request, resp);
+	}
+}
